32e539c85f561b9d3d7ca3e592a143002f8b22e1
Stas Alekseev <stas.alekseev@gmail.com>
2017-08-03 10:40:47 -0400

    Modified the list of dependedn packages on RedHat systems

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,39 +1,11 @@
 class netdata::install inherits netdata {
 
-  $build_deps = [
-    'autoconf-archive',
-    'autoconf',
-    'autogen',
-    'automake',
-    'curl',
-    'gcc',
-    'git',
-    'libmnl-dev',
-    'make',
-    'pkg-config',
-    'uuid-dev',
-    'zlib1g-dev',
-  ]
-
-  # recommended packages for full functionality of netdata
-  $plugin_deps = [
-    'iproute2', # provides the 'tc' application
-    'python',
-    'python-yaml',
-    'python-mysqldb',
-    'python-psycopg2',
-    'nodejs',
-    'lm-sensors',
-    'libmnl0',
-    'netcat'
-  ]
-
   if $netdata::install_dependencies == true {
-    ensure_packages( $build_deps, {'ensure' => 'present'} )
+    ensure_packages( $netdata::dependencies_pkgs, {'ensure' => 'present'} )
   }
 
   if $netdata::install_plugin_dependencies == true {
-    ensure_packages( $plugin_deps, {'ensure' => 'present'} )
+    ensure_packages( $netdata::plugin_dependencies_pkgs, {'ensure' => 'present'} )
   }
 
   if $netdata::install_jq == true {
